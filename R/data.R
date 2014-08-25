da3dc9eb1bf9c49ebccbb947f42e058da0679782
hadley <h.wickham@gmail.com>
2014-08-25 11:42:22 -0500

    Document nutrient_def

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Nutrient definitions.
+#'
+#' @format A data frame with 150 observations and 6 variables:
+#' \describe{
+#' \item{nutr_id}{Unique ID for nutrient}
+#' \item{unit}{Units of measure}
+#' \item{abbr}{International Network of Food Data Systems (INFOODS) tagname}
+#' \item{name}{Name of nutrient/food component}
+#' \item{precision}{Number of decimal places}
+#' \item{sort}{Used to sort nutrient records in the same order as various
+#'  reports produced from SR.}
+#' }
+#' @examples
+#' nutrient_def
+"nutrient_def"
