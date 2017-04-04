ac80f01c09e32968d0b8b79ac2db545c9f426ec2
danpbowen <dan.patrick.bowen@gmail.com>
2017-04-04 16:47:36 -0700

    fix sr26.R parse_file() function calls
    The usda ndb website has changed. Data is stored in a zip file now. Changes here account for the changes to the usda ndb website.

--- data-raw/sr26.R
+++ data-raw/sr26.R
@@ -1,60 +1,61 @@
 library(dplyr)
 library(devtools)
-library(httr)
 
-parse_file <- function(x) {
-  base_url <- "http://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR26/asc/"
-  resp <- GET(paste0(base_url, x))
-
-  text <- content(resp, "text", encoding = "ISO-8859-1")
-  read.delim(text = text, sep = "^", quote = "~", na.strings = c("^^", "~~"),
-    header = FALSE, stringsAsFactors = FALSE) %>% tbl_df()
+parse_file <- function(x, y) {
+  file <- unz(y, x, encoding = "ISO-8859-1")
+  read.delim(file, sep = "^", quote = "~", na.strings = c("^^", "~~"),
+             header = FALSE, stringsAsFactors = FALSE) %>% tbl_df()
 }
 
-food <- parse_file("FOOD_DES.txt")
+temp <- tempfile()
+download.file("https://www.ars.usda.gov/ARSUserFiles/80400525/Data/SR26/dnload/sr26.zip",temp)
+
+food <- parse_file("FOOD_DES.txt", temp)
 names(food) <- c("food_id", "grp_id", "food", "food_abbr", "common", "manufacturer",
-  "survey", "refuse", "ref_pct", "scientific", "n_factor", "pro_factor", "fat_factor",
-  "carb_factor")
+                 "survey", "refuse", "ref_pct", "scientific", "n_factor", "pro_factor", "fat_factor",
+                 "carb_factor")
 food$survey <- food$survey == "Y"
 use_data(food)
 
-food_group <- parse_file("FD_GROUP.txt")
+food_group <- parse_file("FD_GROUP.txt", temp)
 names(food_group) <- c("grp_id", "group")
 use_data(food_group)
 
-nutrient <- parse_file("NUT_DATA.txt")
+nutrient <- parse_file("NUT_DATA.txt", temp)
 names(nutrient) <- c("food_id", "nutr_id", "value", "num_points", "se",
-  "source_type_id", "deriv_id", "impute_id", "fortified", "num_studies", "min",
-  "max", "df", "lwr", "upr", "comments", "modified", "cc")
+                     "source_type_id", "deriv_id", "impute_id", "fortified", "num_studies", "min",
+                     "max", "df", "lwr", "upr", "comments", "modified", "cc")
 nutrient$fortified[nutrient$fortified == ""] <- NA
 use_data(nutrient)
 
-nutrient_def <- parse_file("NUTR_DEF.txt")
+nutrient_def <- parse_file("NUTR_DEF.txt", temp)
 names(nutrient_def) <- c("nutr_id", "unit", "nutr_abbr", "nutr", "precision", "seq")
 use_data(nutrient_def)
 
-source_type <- parse_file("SRC_CD.txt")
+source_type <- parse_file("SRC_CD.txt", temp)
 names(source_type) <- c("source_type_id", "source_type")
 use_data(source_type)
 
-deriv <- parse_file("DERIV_CD.txt")
+deriv <- parse_file("DERIV_CD.txt", temp)
 names(deriv) <- c("deriv_id", "deriv")
 use_data(deriv)
 
-weight <- parse_file("WEIGHT.txt")
+weight <- parse_file("WEIGHT.txt", temp)
 names(weight) <- c("food_id", "seq", "amount", "desc", "weight",
-  "num_points", "sd")
+                   "num_points", "sd")
 use_data(weight)
 
-footnote <- parse_file("FOOTNOTE.txt")
+footnote <- parse_file("FOOTNOTE.txt", temp)
 names(footnote) <- c("food_id", "seq", "type", "nutr_id", "footnote")
 use_data(footnote)
 
-reference <- parse_file("DATA_SRC.txt")
+reference <- parse_file("DATA_SRC.txt", temp)
 names(reference) <- c("ref_id", "authors", "title", "year", "journal", "vol_city",
-  "issue_state", "start_page", "end_page")
+                      "issue_state", "start_page", "end_page")
 use_data(reference)
 
-nutrient_source <- parse_file("DATSRCLN.txt")
+nutrient_source <- parse_file("DATSRCLN.txt", temp)
 names(nutrient_source) <- c("food_id", "nutr_id", "ref_id")
 use_data(nutrient_source)
+
+unlink(temp)
